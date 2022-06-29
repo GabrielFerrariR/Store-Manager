d7592ee7dd4ad79678dfff36924525952ea0f837
Gabriel <gabrielferrari601@gmail.com>
2022-06-29 17:29:55 -0300

    Fix: variaveis de ambiente da connection

--- models/connection.js
+++ models/connection.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: 'db',
-  user: 'root',
-  password: 'password',
+  host: process.env.MYSQL_HOST || 'db',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'password',
   database: 'StoreManager',
 });
 
